6bfce82defcab31b4fcf5c8b87ef46cf68bd1612
jatin_Byteblanket <jkaushik@byteblanket.com>
2021-09-05 17:36:23 +0530

    solutuion 6

---
+++ src/com/company/level1/basicsofprogramming/Digits_Of_A_Number.java
@@ -0,0 +1,15 @@
+package com.company.level1.basicsofprogramming;
+import java.util.*;
+
+public class Digits_Of_A_Number {
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        String words = scn.nextLine();
+        int i = 0;
+        while (i < words.length()){
+            System.out.println(words.charAt[i]);
+            i++;
+        }
+    }
+}
