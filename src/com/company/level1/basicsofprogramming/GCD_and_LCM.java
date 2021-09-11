4199e19e2197ca78577833fca694c429282387fd
jatin_Byteblanket <jkaushik@byteblanket.com>
2021-09-11 19:29:08 +0530

    11 Sept getting Started Completed

---
+++ src/com/company/level1/basicsofprogramming/GCD_and_LCM.java
@@ -0,0 +1,23 @@
+package com.company.level1.basicsofprogramming;
+
+import java.util.*;
+
+public class GCD_and_LCM {
+    public static void main(String [] args){
+        Scanner scn = new Scanner(System.in);
+        int n1 = scn.nextInt();
+        int n2 = scn.nextInt();
+        int gcd = 1;
+        int prod = n1*n2;
+        while(n1%n2 !=0){//jabtak modulus 0 ni aajata
+            int r = n1%n2; // reminder
+            n1 = n2; // gcd rule jisne divide kra vo divide hoga and jo reminder aya tha vo divide krega
+            n2 = r; // thats why reinitializing both n2 divide hoga n1 = n2 and reminder divide krega n2 = r
+//            System.out.println(r + " " + n1 + " " + n2);
+            gcd = r;
+        }
+        int lcm = prod/gcd; // lcm = (n1*n2)/gcd
+        System.out.println(gcd);
+        System.out.println(lcm);
+    }
+}
---
+++ src/com/company/level1/basicsofprogramming/benjamin_bulb.java
@@ -0,0 +1,13 @@
+package com.company.level1.basicsofprogramming;
+
+import java.util.Scanner;
+
+public class benjamin_bulb {
+    public static void main(String [] args){
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    for(int i = 1; i*i<=n; i++){
+        System.out.println(i*i);
+
+    }
+}}
