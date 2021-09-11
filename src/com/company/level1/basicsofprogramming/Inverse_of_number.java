37306a5c0a1a749d69aedad1f3b1f75798d17426
jatin_Byteblanket <jkaushik@byteblanket.com>
2021-09-11 18:40:03 +0530

    11 Sept. Commit 2

--- src/com/company/level1/basicsofprogramming/Inverse_of_number.java
+++ src/com/company/level1/basicsofprogramming/Inverse_of_number.java
@@ -1,4 +1,19 @@
 package com.company.level1.basicsofprogramming;
-
+import java.util.*;
 public class Inverse_of_number {
+    public static void main (String [] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int p =1;
+        int inv = 0;
+        while(n>0){
+            int r = n%10;
+            int q = n/10;
+            n = q;
+            inv = inv + p*(int)Math.pow(10,r-1);
+            p++;
+        }
+        System.out.println(inv);
+    }
 }
+
