89f8d25751b25b413d896766655d2ef0b66e58b0
jatin_Byteblanket <jkaushik@byteblanket.com>
2021-09-12 19:10:51 +0530

    Pattern 1-2-13 done

---
+++ src/com/company/level1/basicsofprogramming/pattern1.java
@@ -0,0 +1,17 @@
+package com.company.level1.basicsofprogramming;
+
+import java.util.Scanner;
+
+public class pattern1 {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        for(int i =1; i<=n;i++){
+            for (int j =1; j<=i; j++){
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+
+    }
+}
