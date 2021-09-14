804657276984e3365a9657b9709d758a34ef5df8
Jatin Kaushik <Jatinchandkaushik@gmail.com>
2021-09-14 15:06:47 +0530

    upto pattern 8 done

--- src/com/company/level1/basicsofprogramming/pattern/pattern6.java
+++ src/com/company/level1/basicsofprogramming/pattern/pattern6.java
@@ -15,7 +15,18 @@ public class pattern6 {
             for (int j =1; j<= nsp; j++){
                 System.out.print("\t");
             }
-            for (int j  =1; j<= nst)
+            for (int j  =1; j<= nst; j++){
+                System.out.print("*\t");
+            }
+            System.out.println();
+            if (i<=n/2){
+                nsp+=2;
+                nst--;}
+            else{
+                nsp -=2;
+                nst++;
+            }
+
         }
     }
 }
